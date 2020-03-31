46b681dbfeee3389f44eb6f04445ca2e6bf418a1
saugat97 <saugat.thapa97@gmail.com>
2020-03-31 17:34:05 -0500

    Updates

---
+++ precache-manifest.da002470a666944fe55f9e9ae8eec12f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7883b554e2bf25f6f60cd2a2122f6e77",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "55fe5232786a940db4e6",
+    "url": "/monsters-rolodex/static/css/main.4d641a21.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55fe5232786a940db4e6",
+    "url": "/monsters-rolodex/static/js/main.46d2a97c.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
